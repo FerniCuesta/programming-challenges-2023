e9a4d9c43a4e32fda0680ab23fb4d78f5ad5ed02
Fernando <fernanby@gmail.com>
2023-02-23 00:03:18 +0100

    Created challenge2

---
+++ challenges/challenge2_tennis_match.py
@@ -0,0 +1,41 @@
+"""
+Español
+
+Escribe un programa que muestre cómo transcurre un juego de tenis y quién lo ha ganado.
+El programa recibirá una secuencia formada por "P1" (Player 1) o "P2" (Player 2), según quien
+gane cada punto del juego.
+
+- Las puntuaciones de un juego son "Love" (cero), 15, 30, 40, "Deuce" (empate), ventaja.
+- Ante la secuencia [P1, P1, P2, P2, P1, P2, P1, P1], el programa mostraría lo siguiente:
+  15 - Love
+  30 - Love
+  30 - 15
+  30 - 30
+  40 - 30
+  Deuce
+  Ventaja P1
+  Ha ganado el P1
+- Si quieres, puedes controlar errores en la entrada de datos.   
+- Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.   
+
+-------------------------------------------------------------------------------
+
+English
+
+Write a programme that shows whow takes place a tennis game and who wins.
+The programme receives a secuence formed by "P1" (Player 1) or "P2" (Player 2), depending on
+who wins each point of the game.
+
+- The puntuations are "Love" (zero), 15, 30, 40, "Deuce" (draw), advantage.
+- With the secuence [P1, P1, P2, P2, P1, P2, P1, P1], the programme would display:
+  15 - Love
+  30 - Love
+  30 - 15
+  30 - 30
+  40 - 30
+  Deuce
+  Advantage P1
+  P1 wins
+- If you want, you can control errors in data entries.
+- Consult the game rules if you have doubts about the puntuation system.
+"""
