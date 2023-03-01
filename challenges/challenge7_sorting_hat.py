656af6c9c49e2daac6855e8684aa6cfe1722e251
Fernando <fernanby@gmail.com>
2023-03-01 13:48:27 +0100

    challenge7 created

---
+++ challenges/challenge7_sorting_hat.py
@@ -0,0 +1,25 @@
+"""
+Español
+
+Crea un programa que simule el comportamiento del sombrero selccionador del
+universo mágico de Harry Potter.
+- De ser posible realizará 5 preguntas (como mínimo) a través de la terminal.
+- Cada pregunta tendrá 4 respuestas posibles (también a selecciona una a través de terminal).
+- En función de las respuestas a las 5 preguntas deberás diseñar un algoritmo que
+  coloque al alumno en una de las 4 casas de Hogwarts (Gryffindor, Slytherin , Hufflepuff y Ravenclaw)
+- Ten en cuenta los rasgos de cada casa para hacer las preguntas y crear el algoritmo seleccionador.
+  Por ejemplo, en Slytherin se premia la ambición y la astucia.
+
+-----------------------------------------------------------------------------
+
+English
+
+Create a programme that imitates the performance of the sorting hat from 
+Harry Potter's magic universe.
+- It will make 5 questions (as least) throught the terminal.
+- Each question will hace 4 possible answers (chosen too throuht the terminal).
+- Depending on the answers to the 5 questions you will design an algorithm that
+put the student in one of the 4 Hogwarts' houses (Gryffindor, Slytherin , Hufflepuff y Ravenclaw).
+- Bear in mind the characteristics of each house to make questions and create the sorting algorithm.
+  For example, in Slytherin is rewarded ambition and astuteness. 
+"""
