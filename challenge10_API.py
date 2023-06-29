b277e5404d68ca0e6d092e9831b031d51dd7273f
FerniCuesta <fernanby@gmail.com>
2023-06-30 00:21:02 +0200

    created challenge10

---
+++ challenge10_API.py
@@ -0,0 +1,17 @@
+"""
+Español
+
+Implementa una llamada HTTP a una API y muestra su resultado a través de la terminal.
+
+Aquí tienes un listado de posibles APIs:
+https://github.com/public-apis/public-apis
+
+---------------------------------------------------------------------------------------------------
+English
+
+Implement a HTTP call to an API and show its result throught terminal.
+
+Here you have a list of possible APIs:
+https://github.com/public-apis/public-apis
+
+"""
